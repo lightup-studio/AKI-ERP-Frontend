203e30bcf341459bc73e7bdc4b9b439904fb38aa
Mason <jeailfy9520@outlook.com>
2023-07-23 17:12:49 +0800

    feat: if user has not token, it will redirect to login page

--- apps/aki-erp/app/(auth)/layout.tsx
+++ apps/aki-erp/app/(auth)/layout.tsx
@@ -1,6 +1,18 @@
+'use client';
+
 import { Container } from '@components/shared/layout';
+import { useRouter } from 'next/navigation';
+
+const token = localStorage.getItem('token');
 
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+  const router = useRouter();
+
+  if (!token) {
+    router.push('/login');
+    return <></>;
+  }
+
   return <Container>{children}</Container>;
 };
 
