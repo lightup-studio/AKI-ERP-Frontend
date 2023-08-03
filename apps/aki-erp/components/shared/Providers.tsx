357514f3865d5eead5dadc02a36e1ca0297db850
Mason <jeailfy9520@outlook.com>
2023-08-03 14:58:00 +0800

    chore: set refetchOnWindowFocus false

--- apps/aki-erp/components/shared/Providers.tsx
+++ apps/aki-erp/components/shared/Providers.tsx
@@ -4,7 +4,11 @@ import store from '@contexts/store';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Provider } from 'react-redux';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: { refetchOnWindowFocus: false },
+  },
+});
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
