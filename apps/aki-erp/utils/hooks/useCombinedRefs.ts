38f0ecb192e77dac17346cb1ab8711b19ec60359
declan.zou <declan.zou@ringcentral.com>
2023-08-06 02:23:28 +0800

    chore: add TODO for team

--- apps/aki-erp/utils/hooks/useCombinedRefs.ts
+++ apps/aki-erp/utils/hooks/useCombinedRefs.ts
@@ -1,5 +1,7 @@
 import { useEffect, useRef } from 'react';
 
+// TODO: suggest not write with that way, useRef can use sync method without useEffect
+// Like https://github.com/michaldudak/material-ui/blob/08272d72b4e7cca8b30ce011f62796f125887ccc/packages/mui-utils/src/useForkRef/useForkRef.ts#L5
 const useCombinedRefs = (...refs: any[]): React.MutableRefObject<any> => {
   const targetRef = useRef();
 
