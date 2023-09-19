712b449c689ed00ee8841fc51e73e402e95348c4
Mason <jeailfy9520@outlook.com>
2023-09-19 16:45:06 +0800

    feat: add preview artwork images

--- apps/aki-erp/components/artworks/ArtworksPreviewBtn.tsx
+++ apps/aki-erp/components/artworks/ArtworksPreviewBtn.tsx
@@ -1,14 +1,43 @@
 'use client';
 
 import { ArtworkDetail, ArtworkMetadata } from '@data-access/models';
-import { Button } from 'react-aria-components';
+import { useRef } from 'react';
 
 interface ArtworksPreviewBtnProps {
   artworks?: ArtworkDetail<ArtworkMetadata>[];
 }
 
-const ArtworksPreviewBtn: React.FC<ArtworksPreviewBtnProps> = () => {
-  return <Button className="btn">預覽</Button>;
+const ArtworksPreviewBtn: React.FC<ArtworksPreviewBtnProps> = ({ artworks }) => {
+  const dialogRef = useRef<HTMLDialogElement>(null);
+
+  return (
+    <>
+      <button className="btn" onClick={() => dialogRef.current?.showModal()}>
+        預覽
+      </button>
+
+      <dialog ref={dialogRef} className="modal">
+        <div className="modal-box">
+          <h3 className="font-bold text-lg mb-4">藝術品</h3>
+          <div className="flex flex-wrap gap-4">
+            {artworks?.map((item) => (
+              <img
+                key={item.id}
+                src={item.imageUrl}
+                alt={item.enName}
+                loading="lazy"
+                className="h-20"
+              />
+            ))}
+          </div>
+        </div>
+
+        <form method="dialog" className="modal-backdrop">
+          <button>close</button>
+        </form>
+      </dialog>
+    </>
+  );
 };
 
 export default ArtworksPreviewBtn;
