7568a82fc745082046498a8a9c6d3cc1378e0669
Mason <jeailfy9520@gmail.com>
2024-02-05 20:42:07 +0800

    feat: add user apis (create & delete)

--- apps/aki-erp/data-access/apis/users.api.ts
+++ apps/aki-erp/data-access/apis/users.api.ts
@@ -1,9 +1,35 @@
 import axios from '@contexts/axios';
-import { User } from '@data-access/models';
+import { Pagination, User } from '@data-access/models';
+
+const url = '/api/Users';
+
+export const fetchUsers = async (queryString?: string): Promise<Pagination<User>> => {
+  const params = new URLSearchParams(queryString);
+  const pageIndex = +(params.get('pageIndex') || 0);
+  const pageSize = +(params.get('pageSize') || 50);
+  const offset = pageIndex * pageSize;
+
+  const res = await axios.get(url);
+  return {
+    data: res.data.slice(offset, offset + pageSize),
+    offset: offset,
+    take: pageSize,
+    pageCount: Math.ceil(res.data.length / pageSize),
+    totalCount: res.data.length,
+  };
+};
 
 export const fetchUsersId = async (id: string): Promise<User> => {
-  const url = '/api/Users';
-
   const res = await axios.get(`${url}/${id}`);
+  return res.data;
+};
+
+export const createUser = async (body?: User): Promise<User> => {
+  const res = await axios.post(url, body);
+  return res.data;
+};
+
+export const deleteUserId = async (id?: number): Promise<void> => {
+  const res = await axios.delete(`${url}/${id}`);
   return res.data;
 };
