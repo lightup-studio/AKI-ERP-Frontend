c60403d9f9c3c43b467f744f5a817a0e1749ffd1
Mason <jeailfy9520@gmail.com>
2024-06-07 23:32:33 +0800

    feat: when store type is RETURNED_SHIPPING and NONE, it must be IN_STOCK

--- apps/aki-erp/utils/showStoreTypeText.ts
+++ apps/aki-erp/utils/showStoreTypeText.ts
@@ -1,9 +1,16 @@
 import { StoreType, storeTypeOptionMap } from '@constants/artwork.constant';
 
 export const showStoreTypeText = (storeType?: StoreType) => {
-  if (storeType === StoreType.RETURNED_LEND_OR_RETURNED_REPAIR) {
+  if (!storeType) return '';
+  if (
+    [
+      StoreType.RETURNED_LEND_OR_RETURNED_REPAIR,
+      StoreType.RETURNED_SHIPPING,
+      StoreType.NONE,
+    ].includes(storeType)
+  ) {
     return storeTypeOptionMap[StoreType.IN_STOCK].label;
   }
 
-  return storeTypeOptionMap[storeType || StoreType.IN_STOCK].label;
+  return storeTypeOptionMap[storeType].label;
 };
