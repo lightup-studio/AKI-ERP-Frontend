9967721d887a95641fb3088ae1a11992b0838a91
Mason <jeailfy9520@gmail.com>
2024-08-01 22:47:51 +0800

    feat: update store type status

--- apps/aki-erp/utils/showStoreTypeText.ts
+++ apps/aki-erp/utils/showStoreTypeText.ts
@@ -1,19 +1,6 @@
 import { StoreType, storeTypeOptionMap } from '@constants/artwork.constant';
 
 export const showStoreTypeText = (storeType?: StoreType) => {
-  if (!storeType) return '';
-  if (
-    [
-      StoreType.LEND,
-      StoreType.REPAIR,
-      StoreType.SHIPPING,
-      StoreType.RETURNED_LEND_OR_RETURNED_REPAIR,
-      StoreType.RETURNED_SHIPPING,
-      StoreType.NONE,
-    ].includes(storeType)
-  ) {
-    return storeTypeOptionMap[StoreType.IN_STOCK].label;
-  }
-
-  return storeTypeOptionMap[storeType].label;
+  if (storeType === StoreType.IN_STOCK) return storeTypeOptionMap[StoreType.IN_STOCK].label;
+  return storeTypeOptionMap[StoreType.NONE].label;
 };
